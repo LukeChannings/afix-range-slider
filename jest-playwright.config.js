a1cbe4e31012f7f8db31db23222b69543ad9337d
Luke Channings <luke@channings.me>
2020-11-05 23:16:41 +0000

    A lot of refactoring and tests

--- jest-playwright.config.js
+++ jest-playwright.config.js
@@ -2,9 +2,10 @@ module.exports = {
   browsers: ["chromium", "webkit", "firefox"],
   launchOptions: {
     headless: true,
+    // slowMo: 500,
   },
   serverOptions: {
-    command: "hammer serve -p 8833 src docs",
+    command: "hammer serve -p 8833 src e2e",
     port: 8833,
   },
 };
--- src/Component.spec.ts
+++ src/Component.spec.ts
@@ -1,86 +1,81 @@
 import pkg from "../package.json";
-import Component from "./Component.js";
-import { toMatchImageSnapshot } from "jest-image-snapshot";
-
-expect.extend({ toMatchImageSnapshot });
+import { AfixRangeSlider } from "./Component";
+import { assert } from "./Component.utils";
 
 describe("afix-range-slider", () => {
-
-  const screenshotOptions = {
-    noColors: true,
-    customSnapshotIdentifier: ({
-      defaultIdentifier,
-    }: {
-      defaultIdentifier: string;
-    }) => `${browserName}-${defaultIdentifier}`,
-  };
-
-  beforeEach(async () => {
-    await page.goto("http://localhost:8833/standalone.html");
-  });
-
   it("registers a component named the same as that package's name", () => {
-    expect(customElements.get(pkg.name)).toBe(Component);
+    expect(customElements.get(pkg.name)).toBe(AfixRangeSlider);
   });
 
-  it("supports setting width and height via CSS", async () => {
-    await page.$eval("afix-range-slider", (el) => {
-      el.style.width = "1234px";
-      el.style.height = "432px";
+  describe("keyboard events", () => {
+    it("handles ArrowRight", () => {
+      const slider = new AfixRangeSlider();
+      slider.value = "50";
+      slider.dispatchEvent(
+        new KeyboardEvent("keydown", {
+          key: "ArrowRight",
+        })
+      );
+      expect(slider.value).toBe("51");
     });
 
-    const value = await(await page.$("afix-range-slider"))?.boundingBox();
-
-    expect(value?.width).toBeCloseTo(1234);
-    expect(value?.height).toBeCloseTo(432);
-  });
-
-  it("instance value is reflected in value attribute", async () => {
-    await page.$eval("afix-range-slider", (el) => {
-      (el as HTMLInputElement).value = "25";
+    it("handles shift-ArrowRight", () => {
+      const slider = new AfixRangeSlider();
+      slider.value = "50";
+      slider.dispatchEvent(
+        new KeyboardEvent("keydown", {
+          key: "ArrowRight",
+          shiftKey: true,
+        })
+      );
+      expect(slider.value).toBe("60");
     });
 
-    const value = await(await page.$("afix-range-slider"))?.getAttribute('value');
-
-    expect(value).toBe("25");
-  });
-
-  it("instance value attribute reflects attribute", async () => {
-    await page.$eval("afix-range-slider", (el) => {
-      el.setAttribute("value", "75");
-    });
-    const value = await page.$eval(
-      "afix-range-slider",
-      (el) => (el as HTMLInputElement).value
-    );
-    expect(value).toBe(75);
-  });
-
-  it("supports line mode", async () => {
-    const sliderEl = await page.$('afix-range-slider')
-
-    page.$eval("afix-range-slider", (el) => {
-      el.setAttribute("line-style", "");
+    it("handles ArrowLeft", () => {
+      const slider = new AfixRangeSlider();
+      slider.value = "50";
+      slider.dispatchEvent(
+        new KeyboardEvent("keydown", {
+          key: "ArrowLeft",
+        })
+      );
+      expect(slider.value).toBe("49");
     });
 
-    expect(sliderEl).toBeTruthy()
-
-    expect(await sliderEl!.screenshot()).toMatchImageSnapshot(
-      screenshotOptions
-    );
-  })
-
-  it("supports horizontal", async () => {
-    const sliderEl = await page.$("afix-range-slider");
-
-    page.$eval("afix-range-slider", (el) => {
-      el.setAttribute("horizontal", "");
+    it("handles shift-ArrowLeft", () => {
+      const slider = new AfixRangeSlider();
+      slider.value = "50";
+      slider.dispatchEvent(
+        new KeyboardEvent("keydown", {
+          key: "ArrowLeft",
+          shiftKey: true,
+        })
+      );
+      expect(slider.value).toBe("40");
     });
 
-    expect(sliderEl).toBeTruthy();
+    it("handles Home", () => {
+      const slider = new AfixRangeSlider();
+      slider.value = "50";
+      slider.dispatchEvent(
+        new KeyboardEvent("keydown", {
+          key: "Home",
+          shiftKey: true,
+        })
+      );
+      expect(slider.value).toBe("0");
+    });
 
-    expect(await sliderEl!.screenshot()).toMatchImageSnapshot(
-      screenshotOptions
-    );
+    it("handles End", () => {
+      const slider = new AfixRangeSlider();
+      slider.value = "50";
+      slider.dispatchEvent(
+        new KeyboardEvent("keydown", {
+          key: "End",
+          shiftKey: true,
+        })
+      );
+      expect(slider.value).toBe("100");
+    });
   });
 });
