310172aaef0b85dcc6113acc91a1f330056ab01c
Luke Channings <luke@channings.me>
2020-11-25 09:04:29 +0000

    Add tests for CSS properties

---
+++ e2e/appearance.spec.ts
@@ -0,0 +1,68 @@
+import { allBrowsersRenderTheSame, setup } from '.'
+
+describe('appearance', () => {
+  test('width & height', async () => {
+    const slider = await setup({ style: 'width: 200px; height: 10px' })
+
+    const boundingBox = (await slider.boundingBox())!
+
+    expect(boundingBox.width).toBe(200)
+    expect(boundingBox.height).toBe(10)
+
+    expect(await slider.screenshot()).toMatchImageSnapshot(
+      allBrowsersRenderTheSame
+    )
+  })
+
+  test('color', async () => {
+    const slider = await setup({ style: 'color: red' })
+
+    expect(await slider.screenshot()).toMatchImageSnapshot(
+      allBrowsersRenderTheSame
+    )
+  })
+
+  test('background-color', async () => {
+    const slider = await setup({
+      'line-style': '',
+      style: 'background-color: pink',
+    })
+
+    expect(await slider.screenshot()).toMatchImageSnapshot(
+      allBrowsersRenderTheSame
+    )
+  })
+
+  test('background-image', async () => {
+    const slider = await setup({
+      value: '0',
+      style: 'background-image: linear-gradient(90deg, red, green)',
+    })
+
+    expect(await slider.screenshot()).toMatchImageSnapshot(
+      allBrowsersRenderTheSame
+    )
+  })
+
+  test('border', async () => {
+    const slider = await setup({
+      'line-style': '',
+      style: 'border: 2px solid red; color: red',
+    })
+
+    expect(await slider.screenshot()).toMatchImageSnapshot(
+      allBrowsersRenderTheSame
+    )
+  })
+
+  test('border-radius', async () => {
+    const slider = await setup({
+      'line-style': '',
+      style: 'border-radius: 1rem; color: red',
+    })
+
+    expect(await slider.screenshot()).toMatchImageSnapshot(
+      allBrowsersRenderTheSame
+    )
+  })
+})
